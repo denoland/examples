d926680ca55da7f1b27a8bb85ba7392a3434e54d
lambtron <andyjiang@gmail.com>
2022-05-19 16:41:23 -0400

    re org

--- with-react/deps.client.js
+++ with-react/deps.client.js
@@ -3,6 +3,3 @@ export * as React from "https://esm.sh/react@alpha";
 export { useEffect, useRef, useState } from "https://esm.sh/react@alpha";
 export * as ReactDOM from "https://esm.sh/react-dom@alpha";
 export * as ReactDOMServer from "https://esm.sh/react-dom@alpha/server?dev";
-
-// nanoid
-export { nanoid } from "https://deno.land/x/nanoid/mod.ts";
---
+++ with-react/src/App.jsx
@@ -0,0 +1,10 @@
+import { React } from "../deps.client.js";
+
+
+export default function App(props) {
+  return (
+    <div>
+      Hello, world!
+    </div>
+  );
+}

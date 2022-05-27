938490ee4d08a0e3037c6c4fc2cabee3b466e484
lambtron <andyjiang@gmail.com>
2022-05-27 15:49:14 -0700

    fix

--- with-react/main.ts
+++ with-react/main.ts
@@ -1,12 +1,9 @@
 import { Application, denoPlugin, esbuild } from "./deps.server.ts";
 
-// const res = await Deno.readDir(".");
-// for await (const v of res) {
-//   console.log(v);
-// }
-
-// const text = await Deno.readTextFile('./with-react/src/index.tsx');
-// console.log(text);
+// Set `cwd` since this file is not in this repo's root directory, which Deno Deploy uses as cwd.
+const cwd = Deno.cwd().includes("with-react")
+  ? Deno.cwd()
+  : `${Deno.cwd()}/with-react`;
 
 // Transpile jsx to js for React.
 await esbuild.default.initialize({
@@ -15,22 +12,17 @@ await esbuild.default.initialize({
 });
 const output = await esbuild.default.build({
   plugins: [denoPlugin()],
-  entryPoints: ["./with-react/src/index.tsx"],
-  // entryPoints: [new URL(`file://${Deno.cwd()}/src/index.tsx`)],
+  entryPoints: ["./src/index.tsx"],
   write: false,
   bundle: true,
   format: "esm",
-  absWorkingDir: Deno.cwd(),
+  absWorkingDir: cwd,
 });
 // The raw transpiled output as a string.
 const indexJs = new TextDecoder().decode(output.outputFiles[0].contents);
 
 // Setup server.
 const app = new Application();
-
-// app.use((ctx) => {
-//   ctx.response.body = "hello, world";
-// });
 
 // Return transpiled script as HTML string.
 app.use((ctx) => {

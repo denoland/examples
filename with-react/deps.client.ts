6338ec2bc93d47bfe2be0c834301e418bbdda036
lambtron <andyjiang@gmail.com>
2022-06-21 09:57:59 -0700

    remove useref

--- with-react/deps.client.ts
+++ with-react/deps.client.ts
@@ -1,6 +1,5 @@
 // react
 export * as React from "https://esm.sh/react@alpha";
-export { useEffect, useRef, useState } from "https://esm.sh/react@alpha";
 export * as ReactDOM from "https://esm.sh/react-dom@alpha";
 export * as ReactDOMServer from "https://esm.sh/react-dom@alpha/server?dev";
 export {

c5b73208d96607c925363a26582f87e3376fa2a3
lambtron <andyjiang@gmail.com>
2022-06-21 09:46:24 -0700

    bump version

--- with-react/deps.server.ts
+++ with-react/deps.server.ts
@@ -2,5 +2,5 @@
 export { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 // esbuild
-export * as esbuild from "https://esm.sh/esbuild-wasm@0.14.42";
+export * as esbuild from "https://esm.sh/esbuild-wasm@0.14.47";
 export { denoPlugin } from "https://deno.land/x/esbuild_deno_loader@0.4.3/mod.ts";

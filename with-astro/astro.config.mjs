66bc66c3a054e3431217abd1c86aa4844bc38d15
Andy Jiang <andyjiang@gmail.com>
2024-10-28 20:19:53 -0700

    add astro

--- with-astro/astro.config.mjs
+++ with-astro/astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "server",
+});
